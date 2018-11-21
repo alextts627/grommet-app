87603c10629257a0fc578d6b3e4c0ab1f5e7b9b2
Tsao <alex.tsao@hpe.com>
2018-11-21 17:46:38 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Heading, Grommet } from 'grommet';
+import { hpe } from 'grommet-theme-hpe';
+import { Notification, Help, User } from 'grommet-icons';
+
+const AppBar = (props) => (
+    <Box
+      tag='header'
+      direction='row'
+      align='center'
+      justify='between'
+      background='brand'
+      pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+      elevation='medium'
+      style={{ zIndex: '100' }}
+      {...props}
+    />
+  );
 
 class App extends Component {
+  state = {
+    showSidebar: false,
+  }
+
   render() {
+    const { showSidebar } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grommet theme={hpe} full>
+      <Box fill>
+        <AppBar>
+          <Heading level='3' margin='none'>Test As As Serice OK</Heading>
+          <AppBar>
+            <Button 
+              icon={<Notification/>}
+              onClick={() => this.setState({ showSidebar: !this.state.showSidebar })}
+            />
+           <Button icon={<User />} onClick={() => {}} />
+           <Button icon={<Help />} onClick={() => {}} />
+          </AppBar>
+        </AppBar>
+        <Box direction='row' flex>
+            <Box flex align='center' justify='center'>
+              app body
+            </Box>
+            {showSidebar && (
+              <Box
+                width='medium'
+                background='light-2'
+                elevation='small'
+                align='center'
+                justify='center'
+              >
+                sidebar
+              </Box>
+            )}
+          </Box>
+        </Box>
+      </Grommet>
     );
   }
 }
